fbd6d64504647a0b8946d46cb28c9a39530b42c6
Dennis-89 <straub.dennis1@web.de>
2024-04-03 21:17:54 +0200

    Next refactor step is done

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -2,6 +2,7 @@ import pygame
 import random
 from pygame import mixer
 from pathlib import Path
+from attr import attrib, attrs
 
 
 WIDTH = 800
@@ -22,7 +23,7 @@ SCORE_TEXT_Y = 10
 START_POSITION_PLAYER_X = 370
 START_POSITION_PLAYER_Y = HIGH - 120
 
-PLAYER_SPEED = 0.4
+PLAYER_SPEED = 1
 BULLET_SPEED = 0.8
 
 NUMBER_OF_ENEMY = 10
@@ -30,76 +31,76 @@ ENEMY_SPEED = 0.3
 ENEMY_STEP_CLOSER = 40
 
 
+@attrs(frozen=False)
+class Figure:
+    x_position = attrib(default=0)
+    y_position = attrib(default=0)
+    move_in_x = attrib(default=0)
+    move_in_y = attrib(default=0)
+    default_direction = attrib(default="+")
+
+    def __attrs_post_init__(self):
+        self.direction = {"+": self.move_in_x, "-": -1 * self.move_in_x}
+
+    def calculate_next_position(self, direction=None):
+        if direction is None:
+            direction = self.default_direction
+        self.x_position += self.direction[direction]
+        if self.x_position < 0 or self.x_position > WIDTH - IMAGE_SIZE_PLAYER[0]:
+            self.x_position = WIDTH - IMAGE_SIZE_PLAYER[0] if direction == "+" else 0
+            if self.move_in_y != 0:
+                self._change_y_position()
+
+    def _change_y_position(self):
+        if self.y_position > HIGH:
+            self.y_position = 50
+        else:
+            self.y_position += ENEMY_STEP_CLOSER
+        self.default_direction = "+" if self.default_direction == "-" else "-"
+
+
+@attrs(frozen=False)
+class Bullet:
+    x_position = attrib(default=0)
+    y_position = attrib(default=START_POSITION_PLAYER_Y)
+    is_ready = attrib(default=True)
+
+    def change_y_position(self):
+        self.y_position -= BULLET_SPEED
+
+
 def clear_screen(screen, background):
     screen.fill((0, 0, 0))
     screen.blit(background, (0, 0))
 
 
-def is_collision(enemy_x, enrmy_y, bullet_x, bullet_y):
-    return ((bullet_x - enemy_x) ** 2 + (bullet_y - enrmy_y) ** 2) ** 0.5 < 27
+def is_collision(enemy, bullet):
+    if bullet.is_ready:
+        return False
+    return (
+        abs(bullet.x_position - enemy.x_position) < 27 and abs(bullet.y_position - enemy.y_position) < 27
+    )
 
 
 def fire_bullet(screen, bullet_image, position):
     screen.blit(bullet_image, position)
 
 
-def move_enemy(screen, enemy_image, position):
-    screen.blit(enemy_image, position)
+def move_figure(screen, image, position):
+    screen.blit(image, position)
 
 
-def move_player(screen, player_image, position):
-    screen.blit(player_image, position)
-
-
-def process_user_input(
-    pygame,
-    event,
-    player_x_change,
-    player_x_position,
-    bullet_is_ready,
-    bullet_y_position,
-    bullet_image,
-    screen,
-):
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            player_x_change -= PLAYER_SPEED
-        elif event.key == pygame.K_RIGHT:
-            player_x_change += PLAYER_SPEED
-        elif event.key == pygame.K_SPACE and bullet_is_ready:
-            fire_bullet(screen, bullet_image, (player_x_position, bullet_y_position))
-            bullet_is_ready = False
-            shut_sound = mixer.Sound("laser.wav")
-            shut_sound.play()
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT:
-            player_x_change += PLAYER_SPEED
-        elif event.key == pygame.K_RIGHT:
-            player_x_change -= PLAYER_SPEED
-    return player_x_change, bullet_is_ready, player_x_position
-
-
-def set_new_enemy_position(enemy_x_position, enemy_y_position, right_way):
-    if right_way:
-        enemy_x_position += ENEMY_SPEED
-    else:
-        enemy_x_position -= ENEMY_SPEED
-    if enemy_x_position <= 0:
-        return 0, enemy_y_position + ENEMY_STEP_CLOSER, True
-    elif enemy_x_position >= WIDTH - IMAGE_SIZE_ENEMY[0]:
-        return WIDTH - IMAGE_SIZE_ENEMY[0], enemy_y_position + ENEMY_STEP_CLOSER, False
-    if enemy_y_position > 600:
-        return enemy_x_position, 50, True
-    return enemy_x_position, enemy_y_position, right_way
-
-
-def set_new_player_position(player_x_position, player_x_change):
-    player_x_position += player_x_change
-    if player_x_position <= 0:
-        return 0
-    elif player_x_position >= WIDTH - IMAGE_SIZE_PLAYER[0]:
-        return WIDTH - IMAGE_SIZE_PLAYER[0]
-    return player_x_position
+def process_user_input(pygame, bullet, player, keys):
+    if keys[pygame.K_LEFT]:
+        player.calculate_next_position("-")
+    elif keys[pygame.K_RIGHT]:
+        player.calculate_next_position("+")
+    elif keys[pygame.K_SPACE] and bullet.is_ready:
+        shut_sound = mixer.Sound("laser.wav")
+        shut_sound.play()
+        bullet.x_position = player.x_position
+        bullet.is_ready = False
+    return bullet
 
 
 def show_score(font, score_value, screen, position):
@@ -112,74 +113,54 @@ def main():
     pygame.display.set_caption("Space Invader")
     screen = pygame.display.set_mode(SCREEN_SIZE)
     background = pygame.image.load(BACKGROUND_IMAGE)
-    background = pygame.transform.scale(background, SCREEN_SIZE)
     pygame.display.set_icon(pygame.image.load(UFO_IMAGE))
     player_image = pygame.transform.scale(
         pygame.image.load(PLAYER_IMAGE), IMAGE_SIZE_PLAYER
     )
-    player_x_change = 0
-    player_x_position = START_POSITION_PLAYER_X
-    player_y_position = START_POSITION_PLAYER_Y
+    player = Figure(START_POSITION_PLAYER_X, START_POSITION_PLAYER_Y, PLAYER_SPEED)
+    enemies = [
+        Figure(
+            random.randint(0, WIDTH - IMAGE_SIZE_ENEMY[0]),
+            random.randint(50, 150),
+            ENEMY_SPEED,
+            ENEMY_STEP_CLOSER,
+        )
+        for _ in range(NUMBER_OF_ENEMY)
+    ]
     enemy_image = pygame.transform.scale(
         pygame.image.load(ENEMY_IMAGE), IMAGE_SIZE_ENEMY
     )
-    enemy_positions = [
-        (random.randint(0, WIDTH - IMAGE_SIZE_ENEMY[0]), random.randint(50, 150), True)
-        for _ in range(NUMBER_OF_ENEMY)
-    ]
     bullet_image = pygame.transform.scale(
         pygame.image.load(BULLET_IMAGE), IMAGE_SIZE_BULLET
     )
-    bullet_x_position = 0
-    bullet_y_position = player_y_position
-    bullet_is_ready = True
     score_value = 0
     font = pygame.font.Font("freesansbold.ttf", 32)
     is_running = True
-
+    bullet = Bullet()
     while is_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
-            player_x_change, bullet_is_ready, bullet_x_position = process_user_input(
-                pygame,
-                event,
-                player_x_change,
-                player_x_position,
-                bullet_is_ready,
-                bullet_y_position,
-                bullet_image,
-                screen,
-            )
+        keys = pygame.key.get_pressed()
+        bullet = process_user_input(pygame, bullet, player, keys)
         clear_screen(screen, background)
-        player_x_position = set_new_player_position(player_x_position, player_x_change)
-        move_player(screen, player_image, (player_x_position, player_y_position))
-        new_positions = []
-        for enemy_x_position, enemy_y_position, right_way in enemy_positions:
-            enemy_x_position, enemy_y_position, right_way = set_new_enemy_position(
-                enemy_x_position, enemy_y_position, right_way
-            )
-
-            if is_collision(
-                enemy_x_position, enemy_y_position, bullet_x_position, bullet_y_position
-            ):
-                bullet_y_position = 480
-                bullet_is_ready = True
+        move_figure(screen, player_image, (player.x_position, player.y_position))
+        for enemy in enemies:
+            enemy.calculate_next_position()
+            if is_collision(enemy, bullet):
+                bullet = Bullet()
                 score_value += 1
-                enemy_x_position = random.randint(0, WIDTH - IMAGE_SIZE_ENEMY[0])
-                enemy_y_position = random.randint(50, 150)
+                enemy.x_position = random.randint(0, WIDTH - IMAGE_SIZE_ENEMY[0])
+                enemy.y_position = random.randint(50, 150)
                 ex_sound = mixer.Sound("explosion.wav")
                 ex_sound.play()
-            move_enemy(screen, enemy_image, (enemy_x_position, enemy_y_position))
-            new_positions.append((enemy_x_position, enemy_y_position, right_way))
-        enemy_positions = new_positions
+            move_figure(screen, enemy_image, (enemy.x_position, enemy.y_position))
 
-        if not bullet_is_ready:
-            fire_bullet(screen, bullet_image, (bullet_x_position, bullet_y_position))
-            bullet_y_position -= BULLET_SPEED
-        if bullet_y_position < -50:
-            bullet_is_ready = True
-            bullet_y_position = player_y_position
+        if not bullet.is_ready:
+            fire_bullet(screen, bullet_image, (bullet.x_position, bullet.y_position))
+            bullet.change_y_position()
+        if bullet.y_position < -50:
+            bullet = Bullet()
         show_score(font, score_value, screen, (SCORE_TEXT_X, SCORE_TEXT_Y))
         pygame.display.update()
 
